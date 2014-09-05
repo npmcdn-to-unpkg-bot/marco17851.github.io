e1f238c9bf466fad86abb630ac3be886ad96ccac
Marco A. Barragan <mbarragan@cs.dartmouth.edu>
2014-09-04 23:14:21 -0400

    Picture Changes
    Changed picture
    Changed colors
    Changed sections
    Need to do:
    Make the contact form work

--- script_functions.js
+++ script_functions.js
@@ -11,50 +11,33 @@ function showSection(num)
 		show=document.getElementById("about_section");
 		if (show.style.display == "none"){
 			show.style.display = "block";
-			header.style.backgroundColor = 'rgba(120, 140, 140, 0.9)';
+			header.style.backgroundColor = 'rgba(30, 40, 50, 0.9)';
 		} else {
 			show.style.display = "none";
-			header.style.backgroundColor = 'rgba(140, 140, 120, 0.7)';
+			header.style.backgroundColor = 'rgba(95, 100, 105, 0.7)';
 		}
 	} else {
 		header=document.getElementById("about_link");
 		show=document.getElementById("about_section");
 		show.style.display = "none";
-		header.style.backgroundColor = 'rgba(140, 140, 120, 0.7)';
+		header.style.backgroundColor = 'rgba(95, 100, 105, 0.7)';
 	}
 	if (num == 2){
 		header=document.getElementById("project_link");
 		show=document.getElementById("project_section");
 		if (show.style.display == "none"){
 			show.style.display = "block";
-			header.style.backgroundColor = 'rgba(120, 140, 140, 0.9)';
+			header.style.backgroundColor = 'rgba(30, 40, 50, 0.9)';
 		} else {
 			show.style.display = "none";
-			header.style.backgroundColor = 'rgba(140, 140, 120, 0.7)';
+			header.style.backgroundColor = 'rgba(95, 100, 105, 0.7)';
 		}
 	} else {
 		header=document.getElementById("project_link");
 		show=document.getElementById("project_section");
 		show.style.display = "none";
-		header.style.backgroundColor = 'rgba(140, 140, 120, 0.7)';
+		header.style.backgroundColor = 'rgba(95, 100, 105, 0.7)';
 	}
-	/*
-	if (num == 3){
-		header=document.getElementById("contact_link");
-		show=document.getElementById("contact_section");
-		if (show.style.display == "none"){
-			show.style.display = "block";
-			header.style.backgroundColor = 'rgba(120, 140, 140, 0.9)';
-		} else {
-			show.style.display = "none";
-			header.style.backgroundColor = 'rgba(140, 140, 120, 0.7)';
-		}
-	} else {
-		header=document.getElementById("contact_link");
-		show=document.getElementById("contact_section");
-		show.style.display = "none";
-		header.style.backgroundColor = 'rgba(140, 140, 120, 0.7)';
-	}*/
 }
 
 function openContactMenu()
